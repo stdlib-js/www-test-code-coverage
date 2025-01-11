2fb7b4ccadafbeff34188d0a7c69a48a0c80fd48
headlessNode <harriskhan047@outlook.com>
2025-01-12 00:10:41 +0500

    feat: add initial infrastructure for test code coverage website

---
+++ tools/make/lib/install/node.mk
@@ -0,0 +1,53 @@
+#/
+# @license Apache-2.0
+#
+# Copyright (c) 2025 The Stdlib Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#/
+
+# VARIABLES #
+
+# Define the path to the root `package.json`:
+ROOT_PACKAGE_JSON ?= $(ROOT_DIR)/package.json
+
+
+# VARIABLES #
+
+#/
+# Installs package dependencies by executing [`npm install`][1].
+#
+# ## Notes
+#
+# -   Packages will be installed in a local `node_modules` directory relative to the project's `package.json` file.
+#
+# [1]: https://docs.npmjs.com/cli/install
+#
+# @example
+# make install-node
+#/
+install-node: $(ROOT_PACKAGE_JSON)
+	$(QUIET) $(NPM) install
+
+.PHONY: install-node
+
+#/
+# Cleans the `node_modules` directory by removing it entirely.
+#
+# @example
+# make clean-node
+#/
+clean-node:
+	$(QUIET) $(DELETE) $(DELETE_FLAGS) $(NODE_MODULES)
+
+.PHONY: clean-node
